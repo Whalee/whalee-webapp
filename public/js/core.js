43725f001c256d460fd27d44d10414035bfd0ac6
gdtnicolas <gdt.nicolas@gmail.com>
2016-01-09 16:09:44 +0100

    Test avec chart

--- public/js/core.js
+++ public/js/core.js
@@ -3,7 +3,7 @@
 
 
         // include ngRoute for all our routing needs
-    var whalee = angular.module('whalee', ['ngRoute']); //,"chart.js"]);
+    var whalee = angular.module('whalee', ['ngRoute']);
 
 
 //    whalee.config(['ChartJsProvider', function (ChartJsProvider) {
@@ -87,6 +87,16 @@ whalee.controller('slaController', function($scope) {
 whalee.controller('projectsController', function($scope, $http, id) {
     $scope.message = 'We are in the projects page of the project '+id;
     $scope.isDeployed = false;
+
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+
+
+
     $scope.deployButtonText = function(){
         return ($scope.isDeployed) ? "Undeploy" : "Deploy";
     };
